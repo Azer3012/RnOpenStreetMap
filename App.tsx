64c45b3d69dce0f05350cce6606126a1d0bed8fe
azer <azer@azers-MacBook-Pro.local>
2023-03-12 21:31:06 +0400

    locations

---
+++ App.tsx
@@ -0,0 +1,20 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+import OpenStreetMap from './components/OpenStreetMap';
+
+
+const App = () => {
+  return (
+    
+      <OpenStreetMap/>
+    
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({
+  
+  
+})
---
+++ components/OpenStreetMap.js
@@ -0,0 +1,52 @@
+import {StyleSheet, Text, TextInput, View} from 'react-native';
+import React from 'react';
+import MapView, {PROVIDER_OPENSTREETMAP, Marker} from 'react-native-maps';
+import {locations} from './values/data';
+
+const OpenStreetMap = () => {
+  return (
+    <View style={styles.container}>
+      <TextInput style={styles.input} />
+      <MapView
+        provider={PROVIDER_OPENSTREETMAP}
+        style={{flex: 1}}
+        initialRegion={{
+          latitude: 40.434416,
+          longitude: 49.911645,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}>
+        {locations.map(marker => (
+          <Marker
+            key={marker.latitude}
+            coordinate={{latitude: marker.latitude, longitude: marker.longitude}}
+            title={marker.title}
+            
+          />
+        ))}
+      </MapView>
+    </View>
+  );
+};
+
+export default OpenStreetMap;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    flex: 1,
+  },
+  input: {
+    position: 'absolute',
+    top: 100,
+    borderWidth: 1,
+    zIndex: 2,
+    width: '80%',
+    backgroundColor: 'white',
+    marginHorizontal: '10%',
+    height: 40,
+    paddingLeft: 15,
+  },
+});
